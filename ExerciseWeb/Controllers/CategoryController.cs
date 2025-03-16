6e8a420ee66cfd7bc022bd7a56cb24df23ea6594
monarulMN <mdmonarulislam720@gmail.com>
2025-03-16 19:28:51 +0600

    Add project files.

---
+++ ExerciseWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ExerciseWeb.Data;
+using ExerciseWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciseWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategories = _dbContext.Categories.ToList();
+            return View(objCategories);
+        }
+    }
+}
---
+++ ExerciseWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExerciseWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
