3f67b6eb6fc724a7b6960f77fd1afdf928799686
monarulMN <mdmonarulislam720@gmail.com>
2025-03-18 00:08:54 +0600

    Initial Commit

--- ExerciseWeb/Controllers/CategoryController.cs
+++ ExerciseWeb/Controllers/CategoryController.cs
@@ -16,5 +16,92 @@ namespace ExerciseWeb.Controllers
             List<Category> objCategories = _dbContext.Categories.ToList();
             return View(objCategories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","The display order can't excetly match the Name");
+            }
+            
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _dbContext.Categories.Find(id);
+            //Category? categoryFromDb = _dbContext.Categories.FirstOrDefault(x => x.Id == id);
+            //Category? categoryFromDb = _dbContext.Categories.Where(x => x.Id == id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+            
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _dbContext.Categories.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteAction(int? id)
+        {
+            Category? category = _dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+
     }
 }
